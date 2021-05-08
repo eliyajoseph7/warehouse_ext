9081e628e52e594245b12f394b37f0c7940426e2
eliyajoseph7 <42994583+eliyajoseph7@users.noreply.github.com>
2021-05-08 10:50:26 +0300

    new route created

--- server/routes/api.php
+++ server/routes/api.php
@@ -40,3 +40,4 @@ Route::get('storage-by-grade', 'Api\v1\Charts\DrawChartController@storageByGrade
 Route::get('warehouse-by-ownership', 'Api\v1\Charts\DrawChartController@storageByOwnership');
 Route::get('stored-crop-and-capacity', 'Api\v1\Charts\DrawChartController@storedCropAndCapacity');
 Route::get('warehouse-capacity-and-crops', 'Api\v1\Charts\DrawChartController@warehouseCapacityAndCrops');
+Route::get('warehouse-utilization', 'Api\v1\Charts\DrawChartController@warehouseUtilization');
