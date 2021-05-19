1acc4a072491c0f0e1504bb3763c303e174a847b
eliyajoseph7 <42994583+eliyajoseph7@users.noreply.github.com>
2021-05-19 15:26:21 +0300

    new routes added

--- server/routes/api.php
+++ server/routes/api.php
@@ -51,3 +51,65 @@ Route::get('warehouse-capacity-and-crops', 'Api\v1\Charts\DrawChartController@wa
 Route::get('warehouse-utilization', 'Api\v1\Charts\DrawChartController@warehouseUtilization');
 Route::get('warehouse-ownership-registration/{type}', 'Api\v1\Charts\DrawChartController@warehouseOwnershipRegistration');
 Route::get('crops-by-location', 'Api\v1\Charts\DrawChartController@getCropsByLocation');
+
+
+// filters
+// only by region
+Route::get('filter-by-region/{regionId}', 'Api\v1\Filters\FilterController@onlyByRegion');
+// only region and district
+Route::get('filter-by-district/{districtId}', 'Api\v1\Filters\FilterController@onlyByRegionAndDistrict');
+// region and ownership
+Route::get('filter-by-region-and-ownership/{regionId}/{ownership}', 'Api\v1\Filters\FilterController@onlyByRegionAndOwnership');
+// region, ownership and registration
+Route::get('filter-by-region-ownership-and-registration/{regionId}/{ownership}/{registration}', 'Api\v1\Filters\FilterController@onlyByRegionOwnershipAndRegistration');
+// except district and crop grade
+Route::get('filter-except-district-and-cropgrade/{regionId}/{ownership}/{registration}/{crop}', 'Api\v1\Filters\FilterController@filterWithExceptionOfDistrictAndCropGrade');
+// except district
+Route::get('filter-except-district/{regionId}/{ownership}/{registration}/{crop}/{grade}', 'Api\v1\Filters\FilterController@filterWithExceptionOfDistrict');
+// region, district and ownership
+Route::get('region-district-ownership/{regionId}/{districtId}/{ownership}', 'Api\v1\Filters\FilterController@filterRegionDistrictOwnership');
+// region, district, ownership and registration
+Route::get('region-district-ownership-registration/{regionId}/{districtId}/{ownership}/{registration}', 'Api\v1\Filters\FilterController@filterRegionDistrictOwnershipRegistration');
+// region, district, ownership, registration and crop
+Route::get('with-district-except-cropgrade/{regionId}/{districtId}/{ownership}/{registration}/{cropId}', 'Api\v1\Filters\FilterController@filterAllExceptCropGrade');
+// all combinations
+Route::get('all-combinations/{regionId}/{districtId}/{ownership}/{registration}/{cropId}/{grade}', 'Api\v1\Filters\FilterController@filterAll');
+
+// district and crop are null
+Route::get('district-and-crop-are-null/{regionId}/{ownership}/{registration}/{grade}', 'Api\v1\Filters\FilterController@districtAndCropAreNull');
+
+// district and registration are null
+Route::get('district-and-registration-are-null/{regionId}/{ownership}/{cropId}/{grade}', 'Api\v1\Filters\FilterController@districtAndRegistrationAreNull');
+
+// district and ownership are null
+Route::get('district-and-ownership-are-null/{regionId}/{registration}/{cropId}/{grade}', 'Api\v1\Filters\FilterController@districtAndOwnershipAreNull');
+
+// district and region are null
+Route::get('district-and-region-are-null/{ownership}/{registration}/{cropId}/{grade}', 'Api\v1\Filters\FilterController@districtAndRegionAreNull');
+
+// district, registration and grade are null
+Route::get('district-registration-and-grade-are-null/{region}/{ownership}/{cropId}', 'Api\v1\Filters\FilterController@districtRegistrationAndGradeAreNull');
+
+// district, ownership and grade are null
+Route::get('district-ownership-and-grade-are-null/{region}/{registration}/{cropId}', 'Api\v1\Filters\FilterController@districtOwnershipAndGradeAreNull');
+
+// district, region and grade are null
+Route::get('district-region-and-grade-are-null/{ownership}/{registration}/{cropId}', 'Api\v1\Filters\FilterController@districtRegionAndGradeAreNull');
+
+// district, registration and crop are null
+Route::get('district-registration-and-crop-are-null/{regionId}/{ownership}/{grade}', 'Api\v1\Filters\FilterController@districtRegistrationAndCropAreNull');
+
+// district, ownership and crop are null
+Route::get('district-ownership-and-crop-are-null/{regionId}/{registration}/{grade}', 'Api\v1\Filters\FilterController@districtOwnershipAndCropAreNull');
+
+// district, region and crop are null
+Route::get('district-region-and-crop-are-null/{ownership}/{registration}/{grade}', 'Api\v1\Filters\FilterController@districtRegionAndCropAreNull');
+
+// district, ownership and registration are null
+Route::get('district-ownership-and-registration-are-null/{regionId}/{crop}/{grade}', 'Api\v1\Filters\FilterController@districtOwnershipAndRegistrationAreNull');
+
+// district, region and registration are null
+Route::get('district-region-and-registration-are-null/{ownership}/{crop}/{grade}', 'Api\v1\Filters\FilterController@districtRegionAndRegistrationAreNull');
+
+// district, region and ownership are null
+Route::get('district-region-and-ownership-are-null/{registration}/{crop}/{grade}', 'Api\v1\Filters\FilterController@districtRegionAndOwnershipnAreNull');
